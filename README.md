62c58eb8bba2361374771f9af3eae085b97ff090
Caleb Sylvest <caleb.sylvest@gmail.com>
2018-03-24 19:46:44 -0500

    service page updates

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# gatsby-starter-default
-The default Gatsby starter
+# Spacetime v2
+Built on the default Gatsby starter
 
 For an overview of the project structure please refer to the [Gatsby documentation - Building with Components](https://www.gatsbyjs.org/docs/building-with-components/)
 
-Install this starter (assuming Gatsby is installed) by running from your CLI:
-```
-gatsby new gatsby-example-site
-```
+## Requirements
+1. Node
+1. Yarn
+1. Babel
+1. Gatsby
+
+## Get Started
+1. Run `yarn` to install dependencies
+1. Run `gatsby develop` to start the local dev server
+  - Error: it's highly likely you will run into dependency issues at some point
+1. Visit `localhost:8000` in the browser
 
 ## Deploy
 
--- src/pages/services.js
+++ src/pages/services.js
@@ -14,27 +14,32 @@ const ServicesPage = ({ transition }) => (
       title={"Services"}
       description={"Whether you’re building a marketing website, a mobile app or the next Facebook, we’re experienced in the right tool for the xxx."} />
 
-    <section className="ph5-l pv5 cf bg-fog-white">
-      <div className="fl w-50-ns">
-        <h2 className="f1 serif red">Our team provides the expertise and skill you need to drive your project forward to success.</h2>
-        <p className="sans-serif black-50 fw5 measure">Unlike our competitors who push a one-size-fits-all solution, we opt for specialized project plans on a per client basis, because your problem is unique and deserves a strategy to drive results and lead to success.</p>
-        <div className="flex">
-          <ul className="lh-copy f6 mv4">
-            <li>UI & UX Design</li>
-            <li>Wireframing</li>
-            <li>Prototyping</li>
-            <li>Front-end Development</li>
-          </ul>
-          <ul className="lh-copy f6 mv4">
-            <li>Product Strategy</li>
-            <li>Technology Consultation</li>
-            <li>Startup Incubation</li>
-            <li>Specially Tailored Retainers</li>
-          </ul>
+    <section className="pv5 cf bg-fog-white">
+      <div className="row">
+        <div className="fl w-50-ns">
+          <h2 className="f1 serif red">Our team provides the expertise and skill you need to drive your project forward to success.</h2>
+          <p className="sans-serif black-50 fw5 measure">Unlike our competitors who push a one-size-fits-all solution, we opt for specialized project plans on a per client basis, because your problem is unique and deserves a strategy to drive results and lead to success.</p>
+          <div className="flex">
+            <ul className="lh-copy f6 mv4">
+              <li>UI & UX Design</li>
+              <li>Wireframing</li>
+              <li>Prototyping</li>
+              <li>Front-end Development</li>
+            </ul>
+            <ul className="lh-copy f6 mv4">
+              <li>Product Strategy</li>
+              <li>Technology Consultation</li>
+              <li>Startup Incubation</li>
+              <li>Specially Tailored Retainers</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <img src="illustrations/services-illustration.png" alt="" className="" />
         </div>
       </div>
     </section>
-    <section>
+    <section className="pv4">
       <div className="row cf">
         <ServiceCard
           name={`design`}
@@ -98,6 +103,33 @@ const ServicesPage = ({ transition }) => (
               icon={`🔮`}
               iconPath={``}
               bgColor={`#ECAC1D`}/>
+      </div>
+    </section>
+
+    <section className="pv4">
+      <div className="row">
+        <div className="br2 ba b--light-blue bg-lightest-blue">
+          <div className="dt-ns dt--fixed-ns w-100">
+            <div className="pa3 pa4-ns dtc-ns v-mid">
+              <div>
+                <h2 className="fw4 blue mt0 mb3">
+                  This is a promo title
+                </h2>
+                <p className="black-70 measure lh-copy mv0">
+                  This is suporting copy for the wonderful promo catchphrase that is going to be used.
+                </p>
+              </div>
+            </div>
+            <div className="pa3 pa4-ns dtc-ns v-mid">
+              <a
+                href="#"
+                className="no-underline f6 tc db w-100 pv3 bg-animate bg-blue hover-bg-dark-blue white br2"
+                >
+                  Sign up for free
+                </a>
+              </div>
+          </div>
+        </div>
       </div>
     </section>
 
