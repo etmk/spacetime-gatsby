f18cc484879c6e23ebe66233bbfe61080bb076c9
Caleb Sylvest <caleb.sylvest@gmail.com>
2018-04-04 13:22:04 -0500

    add netlify plugin. update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,19 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-remark`,
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    // make sure to put last in the array
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 }
