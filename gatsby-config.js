35eebffc7c11371725c545c050266298347896d9
Caleb Sylvest <caleb.sylvest@gmail.com>
2018-05-09 17:20:30 -0500

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,20 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-118963683-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     // `gatsby-plugin-remove-trailing-slashes`,
     `gatsby-plugin-sass`,
