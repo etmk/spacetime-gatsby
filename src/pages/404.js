e23da513de3e9c7861776d0798de5a96aa06e68a
Caleb Sylvest <caleb.sylvest@gmail.com>
2018-06-17 15:35:29 -0500

    update 404 page message

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,21 +10,16 @@ const NotFoundPage = ({transition}) => (
   <div>
     <Helmet title={`404 | ${config.siteTitle}`} />
 
-    <PageHero
-      title={"404"}
-      description={"xxx"} />
-
     <MainWrapper>
 
-      <section className="bg-red pv5 white relative overflow-hidden" style={transition && transition.style}>
-        <div className="row mw8 relative z-1">
-          <h1>NOT FOUND</h1>
-          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-          <h2>404</h2>
-          <hr className="mw2 mh0 mv4" />
-          <h3 className="f2 fw5 lh-title measure-narrow">xxx</h3>
+      <section className="bg-black pv7 white relative overflow-hidden" style={transition && transition.style}>
+        <div className="row mw8 relative z-1 tc">
+
+          <h2 className="f1">404</h2>
+          <p className="white measure-narrow center sans-serif">That means something went wrong (in layman's terms). If you can't find what you're looking for <Link to="/contact/" className="blue hover-blue">send&nbsp;us&nbsp;a&nbsp;message</Link> and we will help!</p>
+
         </div>
-        <img src="/bgs/saturn-bg.jpg" className="w-100 absolute top-0 right-0 z-0" alt="Spacetime background image" />
+        <img src="/bgs/404-lost.gif" className="w-100 absolute top-0 right-0 z-0 o-40" alt="Spacetime background image" />
       </section>
 
     </MainWrapper>
