715382b8e50d6c7ff8cbcbd319e83adaaa4de729
Caleb Sylvest <caleb.sylvest@gmail.com>
2018-09-23 15:29:12 -0500

    update thanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -11,55 +11,61 @@ const ThanksPage = ({ transition }) => (
   <div>
     <Helmet title={`Thanks | ${config.siteTitle}`} />
 
-    <PageHero
-      title={`Thanks`}
-      description={`We've received your message and will get back to you in one hot minute!`} />
+    <section id="PageHero" className="page-hero bg-light-blue" style={transition && transition.style}>
 
-    <section className="pv5 cf bg-fog-white" style={transition && transition.style}>
-      <div className="row">
-        <div className="fl-l w-60-l">
-          <h2 className="f2 f1-ns serif red mt0">Until then&hellip;</h2>
-          <p className="sans-serif black-50 fw5 measure">Check out the rest of our site. Sure, it's not as exciting as talking to a real live human being, but apparently the humans are away and only the robots are tending the station.</p>
+      <div className="page-hero__content row">
+        <h1 className="page-hero__headline">Thank You!</h1>
 
-          <ul className="lh-copy mv4">
-            <li>
-              <Link
-              to="/"
-              title="Home"
-              className="dib fw6 blue">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/portfolio/"
-                title="Portfolio"
-                className="dib fw6 blue">
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/services/"
-                title="Services"
-                className="dib fw6 blue">
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/team/"
-                title="Team"
-                className="dib fw6 blue">
-                Team
-              </Link>
-            </li>
-          </ul>
+        <div className="">
+          <div className="w-70-ns">
+            <h2 className="f2 mt0 fw7">We've received your message and will get back to you in one hot minute! Until then&hellip;</h2>
+            <p className="page-hero__text">
+              Check out the rest of our site.
+            </p>
+            <p className="page-hero__text">
+              Sure, it's not as exciting as talking to a real live human being, but apparently the humans are away and only the robots are tending the station.
+            </p>
+
+            <div className="lh-copy f2 mv4">
+                <Link
+                to="/"
+                title="Home"
+                className="dib fw6 black dim">
+                  🏡 Home
+                </Link><br />
+                <Link
+                  to="/portfolio/"
+                  title="Portfolio"
+                  className="dib fw6 black dim">
+                  💼 Portfolio
+                </Link><br />
+                <Link
+                  to="/services/"
+                  title="Services"
+                  className="dib fw6 black dim">
+                  🛠 Services
+                </Link><br />
+                <Link
+                  to="/team/"
+                  title="Team"
+                  className="dib fw6 black dim">
+                  👩‍🎤 Team
+                </Link>
+                <br />
+                <Link
+                  to="/contact/"
+                  title="Contact"
+                  className="dib fw6 black dim">
+                  💌 Contact
+                </Link>
+            </div>
+
+          </div>
 
         </div>
       </div>
-    </section>
 
+    </section>
 
   </div>
 )
