d05a08973d3ba3fbd8416be92743f0619057efb1
Caleb Sylvest <caleb.sylvest@gmail.com>
2018-12-30 22:17:10 -0600

    remove alice cooper url link. only link client logos if url exist

--- src/components/ClientList/index.js
+++ src/components/ClientList/index.js
@@ -7,10 +7,19 @@ const ClientList = () => (
   <ul className="list ma0 pa0 tc">
     {clients.clients.map(client => (
       <li key={client.name} className="dib w-25-m w-33 pa2">
-        <OutboundLink href={client.url} target="_blank" className="dim">
-          <span className="clip">{client.name}</span>
-          <img src={`${client.logo}`} alt={`${client.name} logo`} />
-        </OutboundLink>
+
+        {client.url.length > 0 ? (
+          <OutboundLink href={client.url} target="_blank" className="dim">
+            <span className="clip">{client.name}</span>
+            <img src={`${client.logo}`} alt={`${client.name} logo`} />
+          </OutboundLink>
+        ) :
+          <div className="dim">
+            <span className="clip">{client.name}</span>
+            <img src={`${client.logo}`} alt={`${client.name} logo`} />
+          </div>
+        }
+
       </li>
     ))}
   </ul>
