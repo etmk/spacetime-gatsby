2aff051f9555fe13bdeba6b8befec6090ec0ed98
Caleb Sylvest <caleb.sylvest@gmail.com>
2019-02-16 23:26:14 -0600

    update Dude Perfect page. SEO optimizations

--- src/pages/portfolio/dude-perfect/index.js
+++ src/pages/portfolio/dude-perfect/index.js
@@ -15,8 +15,8 @@ const DudePerfectPage = () => (
     <Helmet
       title={`Dude Perfect • web design for the unique YouTube influencers`}
       meta={[
-        { name: 'description', content: 'A unique web design experience for the all-star YouTube influencers group Dude Perfect' },
-        { name: 'keywords', content: 'dude perfect, youtube influencers' },
+        { name: 'description', content: 'We worked with YouTube influencers Dude Perfect to design and develop a custom WordPress website as a unique showcase for their videos, games, merch, and partnerships. Go Big!' },
+        { name: 'keywords', content: 'dude perfect, youtube influencers, zurb foundation, wordpress' },
       ]}
       link={[
         { rel: 'canonical', href: `${config.siteUrl}/portfolio/dude-perfect/` }
@@ -26,7 +26,7 @@ const DudePerfectPage = () => (
     <PageHero
       bgColor="#202625"
       bgImage="/portfolio/dude-perfect/dude-perfect-bg.jpg">
-        <img src="/client-logos/dude-perfect-icon-white.svg" className="" alt="Coby Cotton of Dude Perfect" />
+        <img src="/client-logos/dude-perfect-icon-white.svg" className="" alt="Dude Perfect logo" />
         <h1 className="t-headline">Dude Perfect&mdash;<span className="o-40">YouTube influencers specializing in the trick shot</span></h1>
     </PageHero>
 
@@ -40,7 +40,7 @@ const DudePerfectPage = () => (
       <div className="pv5 pv6-ns row narrow">
         <h3 className="f2">Go Big!</h3>
         <p>We worked with Dude Perfect to design and develop a web experience to show off their unique brand, both for fans and partners.</p>
-        <p>Visual and brand design pulled from the existing Dude Perfect legacy, taking into account the years of video content and assets already created. The goal in any project with existing history is to develop a future-forward path, while not forsaking the history and brand equity of the past.</p>
+        <p>Visual and brand design pulled from the existing group legacy, taking into account the years of video content and assets already created. The goal in any project with existing history is to develop a future-forward path, while not forsaking the history and brand equity of the past.</p>
         <p>Together, the Spacetime and Dude Perfect teams developed a fantastic extension of the YouTube influencers ecosystem.</p>
       </div>
 
@@ -53,34 +53,56 @@ const DudePerfectPage = () => (
       </Blockquote>
 
       <div className="bg-banded flex items-start justify-center mv5 mv6-l">
-        <img src="/portfolio/dude-perfect/dude-perfect-youtube-web-design.jpg" className="w-40 w-30-l mh2 mh4-l br2 box-shadow relative" alt="" />
-        <img src="/portfolio/dude-perfect/dude-perfect-website-design.jpg" className="w-40 w-30-l mh2 mh4-l br2 box-shadow relative" alt="" />
+        <img src="/portfolio/dude-perfect/dude-perfect-youtube-web-design.jpg" className="w-40 w-30-l mh2 mh4-l br2 box-shadow relative" alt="Dude Perfect website homepage screenshot" />
+        <img src="/portfolio/dude-perfect/dude-perfect-website-design.jpg" className="w-40 w-30-l mh2 mh4-l br2 box-shadow relative" alt="Dude Perfect website case studies screenshot" />
       </div>
 
       <div className="pv5 pv6-ns row narrow">
         <h3>Audience driven technology</h3>
-        <p>Dude Pefect has <em>a lot of fans</em>. When building for the web and taking into account a wide variety of customer types and situations, you really need to plan ahead for a variety of needs and restrictions. The top being web accessibility, data speeds, interaction on mobile devices, and brand support.</p>
-        <p>To ensure a solid experience across mobile and desktop devices, we developed a custom website using the Foundation by Zurb framework for responsive components. Dude Perfect is built on WordPress and <AniLink cover bg="#21759b" to="/services/wordpress-hosting/">served via our superior WordPress hosting service</AniLink>. Further, we employ heavy asset caching and CDN efforts. This ensures that, despite a media rich web experience, our visitors experience a snappy, fast to load experience every visit.</p>
+        <p>Dude Perfect has <em>a lot of fans</em>. When building for the web and taking into account a wide variety of customer types and situations, you really need to plan ahead for a variety of needs and restrictions. The top being web accessibility, data speeds, interaction on mobile devices, and brand support.</p>
+        <p>To ensure a solid experience across mobile and desktop devices, we developed a custom website using the Foundation by Zurb framework for responsive components. Zurb foundation integrated as a starter theme, called <OutboundLink href="https://foundationpress.olefredrik.com/" target="_blank">FoundationPress</OutboundLink>, allowed us to develop quickly, cleanly, while using a child theme so we are able to easily perform framework updates. WordPress Dude Perfect is built on WordPress and <AniLink cover bg="#21759b" to="/services/wordpress-hosting/">served via our superior WordPress hosting service</AniLink>. Further, we employ heavy asset caching and CDN efforts. This ensures that, despite a media rich web experience, our visitors experience a snappy, fast to load experience every visit.</p>
         <div className="mv5">
           <span className="tc db fw7" style={{color: `#00ffde`, fontSize: `10vw`}}>39M&#43;</span>
           <span className="t-small-caps tc db mv3">Subscribers</span>
         </div>
-        <p>To keep content current and up-to-date, the website hooks into several social APIs to populate current stats and content. Social fans and subscribers are an important metric for a brand like Dude Perfect and for sponsorship partners that wish to work with the team. So, we use APIs and chron jobs to update follower and mobile game download counts daily&mdash;and we built a custom WordPress plugin to sync video and data from YouTube directly into the website.</p>
+        <p>To keep content current and up-to-date, the website hooks into several social APIs to populate current stats and content. Social fans and subscribers are an important metric for YouTube influencers and for sponsorship partners that wish to work with the team. So, we use APIs and chron jobs to update follower and mobile game download counts daily&mdash;and we built a custom WordPress plugin to sync video and data from YouTube directly into the website.</p>
       </div>
 
       <div className="bg-banded band-centered flex items-start justify-center mv4 mv6-l">
-        <img src="/portfolio/dude-perfect/dude-perfect-mobile-web-design.jpg" className="w-25 mh2 mh4-l relative" alt="" />
-        <img src="/portfolio/dude-perfect/dude-perfect-mobile-website.jpg" className="w-25 mh2 mh4-l relative" alt="" />
-        <img src="/portfolio/dude-perfect/dude-perfect-mobile-web-graphics.jpg" className="w-25 mh2 mh4-l relative" alt="" />
+        <img src="/portfolio/dude-perfect/dude-perfect-mobile-web-design.jpg" className="w-25 mh2 mh4-l relative" alt="Mobile view of the Dude Perfect about page" />
+        <img src="/portfolio/dude-perfect/dude-perfect-mobile-website.jpg" className="w-25 mh2 mh4-l relative" alt="Mobile view of the Dude Perfect press page" />
+        <img src="/portfolio/dude-perfect/dude-perfect-mobile-web-graphics.jpg" className="w-25 mh2 mh4-l relative" alt="Mobile view of the Dude Perfect videos page" />
       </div>
 
-
+      <div className="row narrow pv5 pv6-ns">
+        <h3>Tools of the trade</h3>
+        <p>Building a website is easy. Building a system that runs and grows on auto-pilot, updates and optimizes data and code, and serves tens of thousands of monthly visitors at blazing fast speeds&mdash;well, that ain&rsquo;t nothing to sneeze at.</p>
+        <p>Here are some of the tools in our toolbox of tricks:</p>
+        <ul className="lh-copy">
+          <li>Web design</li>
+          <li>Wireframing</li>
+          <li>User experience design (UX)</li>
+          <li>WordPress theme development</li>
+          <li>WordPress hosting</li>
+          <li>Frontend development</li>
+          <li>YouTube integration</li>
+          <li>Technical SEO</li>
+          <li>SEO Audit</li>
+          <li>Mobile first development</li>
+          <li>Responsive web design (RWD)</li>
+          <li>Zurb Foundation</li>
+          <li>Shopify merch</li>
+          <li>Google Analytics</li>
+          <li>Google Search Console</li>
+          <li>MailChimp integration</li>
+        </ul>
+      </div>
 
       <div className="bg-athens-gray mv5 pt5 pt6-l pb5-l relative overflow-hidden">
         <div className="row flex flex-column flex-row-reverse-l justify-end-l">
           <div className="w-30-l">
             <h3>YouTube and beyond</h3>
-            <p>Over the years, Dude Perfect has expanded beyond YouTube&mdash;with merch, mobile games, books, and even a tv show. The incredible talent and creativity from the Dude Perfect team seems inexhaustible with each new idea and venture they undertake.</p>
+            <p>Over the years, Dude Perfect has expanded beyond YouTube&mdash;with merch, mobile games, books, and even a tv show. The incredible talent and creativity from the team seems inexhaustible with each new idea and venture they undertake.</p>
           </div>
           <div className="w-60-l mt4 mt0-l">
             <img src="/portfolio/dude-perfect/dude-perfect-mobile-games.jpg" className="nb6 nb3-m nb5-l" alt="" />
